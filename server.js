2d82833289513602b1451dc037e309bfa61ca99e
Omer <eldaromer@gmail.com>
2018-07-12 16:09:26 -0600

    Authentication up and running

--- server.js
+++ server.js
@@ -4,6 +4,13 @@ const Router = require('koa-router');
 const cors = require('@koa/cors');
 const bodyParser = require('koa-bodyparser');
 const Config = require('./Config');
+const ipFilter = require('koa-ip');
+const {google} = require('googleapis');
+const auth = require('./auth');
+
+auth((authClient) => {
+    console.log(authClient)
+});
 
 const app = new Koa();
 let router = new Router();
@@ -11,10 +18,16 @@ let router = new Router();
 app.use(logger());
 app.use(bodyParser());
 app.use(cors());
+/*app.use(ipFilter({
+    whitelist: ['207.97.227.253', '50.57.128.197', '108.171.174.178'],
+    handler: async(ctx) => {
+        ctx.status = 403
+    }
+}));*/
+
 
 router.post('/hook', (ctx, next) => {
     const body = ctx.request.body;
-    console.log(body.pusher);
 
     if (!body.pusher) {
         ctx.status = 422;
@@ -41,12 +54,35 @@ router.post('/hook', (ctx, next) => {
         return;
     }
 
-
     let subject = 'Re: [' + body.repository.full_name + '] ' + body.head_commit.message;
-    console.log(subject);
+
+    let html = getHtml(body);
+    console.log(html);
+    let text = getText(body);
+    console.log(text);
+
+
+    ctx.status = 200;
+    ctx.body = {
+        "passed": true
+    }
+});
+
+let getText = (body) => {
+    let content = '@' + body.sender.login + ' pushed ' + body.commits.length
+        + (body.commits.length > 1 ? ' commits\n': ' commit.\n');
+
+    for (let commit of body.commits) {
+        content+='\n' + commit.id.substr(0,7) + '  ' + commit.message;
+    }
+
+    return content
+};
+
+let getHtml = (body) => {
 
     let content = '<p><a href="' + body.sender.html_url + '" class="user-mention">@' + body.sender.login +'</a> pushed '
-        + body.commits.length + ' ' + (body.commits.length > 1 ? 'commits.' : 'commit.') + '</p>\n\n' +
+        + body.commits.length + (body.commits.length > 1 ? ' commits.' : ' commit.') + '</p>\n\n' +
         '<ul>';
 
     for (let commit of body.commits) {
@@ -56,13 +92,9 @@ router.post('/hook', (ctx, next) => {
 
     content += '</ul>';
 
-    console.log(content);
+    return content;
+};
 
-    ctx.status = 200;
-    ctx.body = {
-        "passed": true
-    }
-});
 
 app.use(router.routes());
 app.use(router.allowedMethods());
